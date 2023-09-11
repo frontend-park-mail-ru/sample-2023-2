0d2a7e7360eca4c7daf9b62f622403afe57bb98e
Mikhail Volynov <m.volynov@corp.mail.ru>
2023-09-11 11:28:03 +0300

    Add lesson-1 practice

---
+++ public/index.js
@@ -0,0 +1,7 @@
+const header = document.getElementById('header');
+
+header.addEventListener('click', function () {
+    header.style.backgroundColor = 'pink';
+});
+
+console.log('Привет! Вы восхитительны!')
